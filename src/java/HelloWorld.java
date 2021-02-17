26dd368c072f87a6edccafaa6bcff1b3cf50c2da
Evy Dekkers <17366381+evyd13@users.noreply.github.com>
2021-02-17 10:09:33 +0100

    Make Hello World OO

--- src/java/HelloWorld.java
+++ src/java/HelloWorld.java
@@ -4,14 +4,8 @@ import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
 public class HelloWorld {
 
     public static void main(String[] args) throws Exception {
-        String robotUrl = "tcp://nao.local:9559";
-        // Create a new application
-        Application application = new Application(args, robotUrl);
-        // Start your application
-        application.start();
-        // Create an ALTextToSpeech object and link it to your current session
-        ALTextToSpeech tts = new ALTextToSpeech(application.session());
-        // Make your robot say something
-        tts.say("Hello World!");
+        NAO nao = new NAO();
+        nao.connect("localhost", 1234);
+        nao.say("Hello world!");
     }
 }

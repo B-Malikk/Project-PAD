ac0b5fd917b02140b19e4ffd133d8f8d9a528846
Evelien Dekkers <evelien.dekkers@hva.nl>
2021-04-13 12:44:02 +0200

    Update humangreeter

--- app/scripts/humangreeter.py
+++ app/scripts/humangreeter.py
@@ -2,12 +2,11 @@
 # -*- encoding: UTF-8 -*-
 
 """Example: A Simple class to get & read FaceDetected Events"""
-#Threading
+
 import qi
 import time
 import sys
 import argparse
-import threading
 
 from naoqi import ALProxy
 
@@ -19,10 +18,34 @@ tts = ALProxy("ALTextToSpeech", "mirai.robot.hva-robots.nl",9559)
 posture_service = ALProxy("ALRobotPosture", "mirai.robot.hva-robots.nl",9559)
 tts.setLanguage("Dutch")
 
+tts.say("kaas")
+
+def wave():
+    # Wave met linker arm
+    tts.say("Hallo!")
+    names = ["LShoulderRoll", "LShoulderPitch", "LWristYaw"]
+    elbow = ["LElbowRoll"]
+    angleLists = [1.6, -0.5, -0.5]
+    angleElbow = [-2.0, -0.5, -2, 0]
+    times = [0.4, 0.6, 0.8, 1.0]
+    elbowtimes = [0.2, 0.6, 1.0, 1.4]
+    isAbsolute = True
+    joint.angleInterpolation(names, angleLists, times, isAbsolute)
+    joint.angleInterpolation(elbow, angleElbow, elbowtimes, isAbsolute)
+    posture_service.goToPosture("StandInit", 0.7)
+    tts.say("Welcom in het Wibauthuis")
+    global x
+    x = False
+
+
+
 class HumanGreeter(object):
     """
     A simple class to react to face detection events.
     """
+    is_greeting = False
+    got_face = False
+
 
     def __init__(self, app):
         """
@@ -46,33 +69,36 @@ class HumanGreeter(object):
         """
         Callback for event FaceDetected.
         """
-        print(value)
-        if value == []:  # empty value when the face disappears
-            #self.got_face = False
-            test = True
-        elif not self.got_face:  # only speak the first time a face appears
-            self.got_face = True
-            print "I saw a face!"
-            self.greetThread()  # wave if a face is
+        if not self.got_face and value != []:  # only speak the first time a face appears
+            if not self.is_greeting:
+                self.is_greeting = True
+                self.got_face = True
+                print "I saw a face!"
+                wave()  #wave if a face is seen
 
-            # First Field = TimeStamp.
-            timeStamp = value[0]
-            print "TimeStamp is: " + str(timeStamp)
 
-            # Second Field = array of face_Info's.
-            faceInfoArray = value[1]
-            for j in range(len(faceInfoArray) - 1):
-                faceInfo = faceInfoArray[j]
 
-                # First Field = Shape info.
-                faceShapeInfo = faceInfo[0]
+                # First Field = TimeStamp.
+                timeStamp = value[0]
+                print "TimeStamp is: " + str(timeStamp)
 
-                # Second Field = Extra info (empty for now).
-                faceExtraInfo = faceInfo[1]
+                # Second Field = array of face_Info's.
+                faceInfoArray = value[1]
+                for j in range(len(faceInfoArray) - 1):
+                    faceInfo = faceInfoArray[j]
 
-                print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
-                print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
-                print "Face Extra Infos :" + str(faceExtraInfo)
+                    # First Field = Shape info.
+                    faceShapeInfo = faceInfo[0]
+
+                    # Second Field = Extra info (empty for now).
+                    faceExtraInfo = faceInfo[1]
+
+                    print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
+                    print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
+                    print "Face Extra Infos :" + str(faceExtraInfo)
+                self.is_greeting = False
+
+        self.got_face = (value)
 
     def run(self):
         """
@@ -81,43 +107,14 @@ class HumanGreeter(object):
 
         print "Starting HumanGreeter"
         try:
-            while True and x == True:
-                time.sleep(1)
+            while True:
+                time.sleep(.01)
         except KeyboardInterrupt:
             print "Interrupted by user, stopping HumanGreeter"
             self.face_detection.unsubscribe("HumanGreeter")
             # stop
             sys.exit(0)
 
-    def wave(self):
-        print("thread 1 started")
-        # Wave met linker arm
-        tts.say("Hallo!")
-        names = ["LShoulderRoll", "LShoulderPitch", "LWristYaw"]
-        elbow = ["LElbowRoll"]
-        angleLists = [1.6, -0.5, -0.5]
-        angleElbow = [-2.0, -0.5, -2, 0]
-        times = [1.0, 1.2, 1.4, 1.6]
-        elbowtimes = [1.6, 2.0, 2.4, 2.8]
-        times = [0.4, 0.6, 0.8, 1.0]
-        elbowtimes = [0.2, 0.6, 1.0, 1.4]
-        isAbsolute = True
-        joint.angleInterpolation(names, angleLists, times, isAbsolute)
-        joint.angleInterpolation(elbow, angleElbow, elbowtimes, isAbsolute)
-        posture_service.goToPosture("StandInit", 0.5)
-        posture_service.goToPosture("StandInit", 0.7)
-        global x  # class niet dit
-        x = False
-
-    def greet(self):
-        print("Thread 2 started ")
-        tts.say("Welkom in het Wibauthuis")
-
-    def greetThread(self):
-        t1 = threading.Thread(target=self.wave)
-        t2 = threading.Thread(target=self.greet)
-        t2.start()
-        t1.start()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()

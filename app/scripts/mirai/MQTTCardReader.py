e2d3a4b802a23cb07842c5221a509ed947dbc606
Ceren <ceren.kayapinar@hva.nl>
2021-05-01 18:12:38 +0200

    Database verbinding

--- app/scripts/mirai/MQTTCardReader.py
+++ app/scripts/mirai/MQTTCardReader.py
@@ -9,6 +9,7 @@ class BaseModel(Model):
     class Meta:
         database = mydb
 
+
 # Defines table 'User'
 class User(BaseModel):
     id = AutoField(primary_key=True)
@@ -18,7 +19,10 @@ class User(BaseModel):
 
     class Meta:
         db_table = 'User'
+
+
 mydb.create_tables([User, ])
+
 
 class MQTTCardReader(MQTTListenerBaseClass):
     topic = 'Mirai/card/#'
@@ -27,18 +31,10 @@ class MQTTCardReader(MQTTListenerBaseClass):
         print(msg.topic + " " + str(msg.payload))
         if msg.topic == 'Mirai/card/scan':
             pass
+        user, created = User.get_or_create(nummer=msg.payload)
+        user.inklokken = True if user.inklokken == False else False
+        user.save()
 
-        ids = []
-        for msg.payload in ids:
-            if not msg.payload in ids:
-                print("not in array yet")
-                ids.append(msg.payload)
-                User.create(nummer='10')
-            else:
-                print("already in array")
-                ids.remove(msg.payload)
-                User.delete().where(User.nummer == '10')
-        print(ids)
 
 # reader = MQTTCardReader(None)
 # reader.on_scan()

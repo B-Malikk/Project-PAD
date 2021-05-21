e0b70d4687ea0b6a59cd1a9c78791111c0ba320b
Ceren <ceren.kayapinar@hva.nl>
2021-05-21 11:53:58 +0200

    update scanner motion

--- app/scripts/mirai/Dialog.py
+++ app/scripts/mirai/Dialog.py
@@ -1,46 +1,40 @@
 import time
+
+
 class Dialog(object):
 
-
-    def __init__(self,mirai):
+    def __init__(self, mirai):
         self._dialogService = mirai.getProxy('ALDialog')
         self._topicName = None
-        self._dialogStarted=False
+        self._dialogStarted = False
         # Getting the service ALDialog
 
-    def set_language(self,language ):
+    def set_language(self, language):
         self._dialogService.setLanguage(language)
 
-
     # Starting the dialog engine - we need to type an  string as the identifier
-    def start_dialog(self,language, topic_name):
+    def start_dialog(self, language, topic_name):
         self._dialogStarted = True
         self.set_language(language)
         self._topicName = self._dialogService.loadTopicContent(topic_name)
         # Activating the loaded topics
         self._dialogService.activateTopic(self._topicName)
-        #self.dialog_service.say(self.topic_name)
+        # self.dialog_service.say(self.topic_name)
         print self._dialogService.getActivatedTopics()
         self._dialogService.subscribe('dialog')
 
         while self._dialogStarted:
             time.sleep(1)
-            #try:
-                #raw_input("\nSpeak to the robot using rules from both the activated topics. Press Enter when finished:")
-            #finally:
-                #self.stop_dialog(self._topicName)
+            # try:
+            # raw_input("\nSpeak to the robot using rules from both the activated topics. Press Enter when finished:")
+            # finally:
+            # self.stop_dialog(self._topicName)
 
-        #self.dialog_service.deactivateTopic(topicName)
-        #self.dialog_service.unloadTopic(topicName)
-
-
-
-
-
-
+        # self.dialog_service.deactivateTopic(topicName)
+        # self.dialog_service.unloadTopic(topicName)
 
     # stopping the dialog engine
-    def stop_dialog(self,topic_name):
+    def stop_dialog(self, topic_name):
         # Deactivating all topics
         self._dialogStarted = False
         self._dialogService.unsubscribe('dialog')
@@ -49,4 +43,3 @@ class Dialog(object):
         # we can unload all topics and free the associated memory
         self._dialogService.unloadTopic(topic_name)
         self._dialogService.unsubscribe('dialog')
-

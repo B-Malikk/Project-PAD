9ac5f86af1544b9b11a1158d30678e6819036cd2
Ceren <ceren.kayapinar@hva.nl>
2021-05-26 14:08:19 +0200

    vocab engels aangemaakt

--- app/scripts/mirai/MQTTWebListener.py
+++ app/scripts/mirai/MQTTWebListener.py
@@ -4,7 +4,7 @@ from Conversation import Dialog
 
 class MQTTWebListener(MQTTListenerBaseClass):
     topic = 'Mirai/web/#'
-    topic_content_1 = ('topic: ~topic30()\n'
+    topic_content_nl = ('topic: ~topic30()\n'
                        'language: dun\n'
                        'concept:(Help) [help vraag]\n'
                        'u: (Ik heb hulp nodig) Ik hoor graag je vraag!\n'
@@ -14,13 +14,23 @@ class MQTTWebListener(MQTTListenerBaseClass):
                        'u: (Waar is de balie?) De balie is aan de linker kant van mij'
                        'u: (Waar ben ik?) Je bent in het Wibauthuis\n')
 
-    # Speechrecognition
-    vocabularyNl = ['ja', 'nee', 'help mij', 'ik ben mijn pasje kwijt', 'Ik weet niet waar mijn lokaal is',
-                  'Ik heb hulp nodig', 'Hoe gaat het?', 'Waar is de balie?', 'Waar ben ik?']
+    topic_content_en = ('topic: ~topic40()\n'
+                       'language: enu\n'
+                       'concept:(Help) [help question]\n'
+                       'u: (I need your help) I would love to help you!\n'
+                       'u: (How are you doing?) I am doing amazing.\n'
+                       'u: (I lost my school ID) You can talk to someone at the information desk about your school ID\n'
+                       'u: (I dont know where my classroom is) You can find the route to your classroom on my tablet\n'
+                       'u: (Where is the information desk?) The information desk is on my left\n'
+                       'u: (Where am I?) You are in the Wibauthouse\n')
 
     # Speechrecognition
-    vocabularyEn = ['ja', 'nee', 'help mij', 'ik ben mijn pasje kwijt', 'Ik weet niet waar mijn lokaal is',
-                  'Ik heb hulp nodig', 'Hoe gaat het?']
+    vocabularyNl = ['ik ben mijn pasje kwijt', 'Ik weet niet waar mijn lokaal is',
+                    'Ik heb hulp nodig', 'Hoe gaat het?', 'Waar is de balie?', 'Waar ben ik?']
+
+    # Speechrecognition
+    vocabularyEn = ['I need your help', 'How are you doing?', 'I lost my school ID',
+                    'I dont know where my classroom is', 'Where is the information desk?', 'Where am I?']
 
     def on_event(self, msg):
         print(msg.topic + " " + str(msg.payload))
@@ -32,15 +42,15 @@ class MQTTWebListener(MQTTListenerBaseClass):
             self.mirai.textToSpeech.say("je bent nu in het HVA Wibauthuis aan de wibautstraat.")
         elif str(msg.payload) == 'terug':
             self.mirai.robotState.setPosture('open')
-            self.mirai.Dialog.deactivateTopic(self.topic_content_1)
+            self.mirai.Dialog.deactivateTopic(self.topic_content_nl)
         elif str(msg.payload) == 'dutch':
             self.mirai.robotState.setPosture('dialog')
             self.mirai.textToSpeech.setLanguage("Dutch")
             self.mirai.speechRecognition.setVocabulary(self.vocabularyNl)
             self.mirai.textToSpeech.say("selecteer een probleem of roep het naar mij")
-            self.mirai.Dialog.activateTopic(self.topic_content_1,"Dutch")
+            self.mirai.Dialog.activateTopic(self.topic_content_nl, "Dutch")
 
-            #self.mirai.dialog.start_dialog("Dutch",self.topic_content2)
+            # self.mirai.dialog.start_dialog("Dutch",self.topic_content2)
 
         elif str(msg.payload) == 'card':
             self.mirai.textToSpeech.say("I will refer you to the counter.")
@@ -50,10 +60,10 @@ class MQTTWebListener(MQTTListenerBaseClass):
             self.mirai.textToSpeech.say("only enter your classroom number.")
         elif str(msg.payload) == 'back':
             self.mirai.robotState.setPosture('open')
-            pass
+            self.mirai.Dialog.deactivateTopic(self.topic_content_en)
         elif str(msg.payload) == 'english':
             self.mirai.robotState.setPosture('dialog')
             self.mirai.textToSpeech.setLanguage("English")
             self.mirai.speechRecognition.setVocabulary(self.vocabularyEn)
-            self.mirai.Dialog.activateTopic(self.topic_content_1, "English")
             self.mirai.textToSpeech.say("select a problem or ask me")
+            self.mirai.Dialog.activateTopic(self.topic_content_en, "English")

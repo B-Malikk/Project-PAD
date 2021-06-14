850163612e13b40a54a97a6e33ea52b512e5d451
Ceren <ceren.kayapinar@hva.nl>
2021-06-14 12:35:58 +0200

    comments toegevoegd aan documentatie

--- app/mirai/Conversation.py
+++ app/mirai/Conversation.py
@@ -7,13 +7,20 @@ class Dialog(object):
         self._topicName = None
         self._dialogStarted = False
 
+    # Activates the dialog with the chosen topic
     def activateTopic(self, topiccontent, language):
+        # sets the spoken language to desired language
         self._proxy.setLanguage(language)
+        # Loads the chosen topic and puts it in a variable
         topicname = self._proxy.loadTopicContent(topiccontent)
+        # activates chosen topic
         self._proxy.activateTopic(topicname)
+        #starts the dialog engine
         self._proxy.subscribe('my_dialog_example')
 
+    # deactivates dialog
     def deactivateTopic(self, topiccontent):
+        # Will try to stop the dialog unless an error occurs
         try:# stopping the dialog engine
             self._proxy.unsubscribe('my_dialog_example')
 
@@ -21,7 +28,6 @@ class Dialog(object):
             self._proxy.deactivateTopic(topiccontent)
 
             # now that the dialog engine is stopped and there are no more activated topics,
-            # we can unload all topics and free the associated memory
+            # all topics are unloaded and the associated memory is emptied
             self._proxy.unloadTopic(topiccontent)
-            #mqtt listner back = stop
         except: pass
